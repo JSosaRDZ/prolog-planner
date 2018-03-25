e1d25d6fe67bebe3c8e4b1115c1161515c2cc420
Luis Jimenez <luis.gamarra26@gmail.com>
2018-03-25 13:37:27 -0400

    Formatted both two room planner solutions as directed from the program 2 description. Both solutions provide the same output and has been validated to be correct.

--- tworoomplanner.pl
+++ tworoomplanner.pl
@@ -1,21 +1,16 @@
-
-%%%%%%%%% Simple Prolog Planner %%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%
-%%% Based on one of the sample programs in:
-%%%
-%%% Artificial Intelligence:
-%%% Structures and strategies for complex problem solving
-%%%
-%%% by George F. Luger and William A. Stubblefield
-%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+/*
+* CAP 4630 Artificial Intelligence
+* University of Central Florida
+* Spring 2018
+* Program 2 - Prolog Two Room Planner
+* Written by: Luis Gamarra Jimenez and Jose Sosa
+*/
 
 /* module */
 :- module( planner,
         [
-            plan/4,change_state/3,conditions_met/2,member_state/2,
-            move/3,go/2,testSingleRoomSlides/0,
-            testTwoRoomSlides/0
+            plan/4, change_state/3, conditions_met/2, member_state/2,
+            move/3, go/2, test1/0, test2/0, test3/0
         ]).
 
 /* utils */
@@ -87,13 +82,13 @@ move(
     ).
 
 move(
-     stack(X, Y, roomlocation1),
+     stack(X, Y),
      [holding(X), clear(Y, roomlocation1), roomlocation1],
      [del(holding(X)), del(clear(Y, roomlocation1)), add(handempty), add(on(X, Y, roomlocation1)), add(clear(X, roomlocation1))]
     ).
 
 move(
-     stack(X, Y, roomlocation2),
+     stack(X, Y),
      [holding(X), clear(Y, roomlocation2), roomlocation2],
      [del(holding(X)), del(clear(Y, roomlocation2)), add(handempty), add(on(X, Y, roomlocation2)), add(clear(X, roomlocation2))]
     ).
@@ -113,7 +108,41 @@ move(
 /* run commands */
 go(S, G) :- plan(S, G, [S], []).
 
-testSingleRoomSlides :- go(
-            [handempty, ontable(c,roomlocation1), ontable(b,roomlocation1), on(a, b, roomlocation1), clear(a, roomlocation1), clear(c, roomlocation1), roomlocation1],
-            [handempty, ontable(c,roomlocation1), on(b, c,roomlocation1), on(a, b,roomlocation1), clear(a, roomlocation1), roomlocation1]
-                          ).
+/**
+* test1 from Program 2 Requirements section.
+* test1 is a 0-arity test predicate.
+* The start state is: block A, B, and C and robotic arm are all in Room 1.
+*                     Block B and C on the table and block A on top of block B.
+* The goal state is: all blocks and arm in Room 1, with block C on the table,
+*                    block B on C, and block A on B.
+*/
+test1 :- go(
+            [handempty, ontable(b, roomlocation1), ontable(c, roomlocation1), on(a, b, roomlocation1), clear(a, roomlocation1), clear(c, roomlocation1), roomlocation1],
+            [handempty, ontable(c, roomlocation1), on(b, c, roomlocation1), on(a, b, roomlocation1), clear(a, roomlocation1), roomlocation1]
+            ).
+
+/**
+* test2 from Program 2 Requirements section.
+* test2 is a 0-arity test predicate.
+* The start state is: block A, B, and C and robotic arm are all in Room 1.
+*                     Block B and C on the table and block A on top of block B.
+* The goal state is: robot arm in Room 1, all blocks in Room 2, with block B on the table,
+                     block C on B, and block A on C.
+*/
+test2 :- go(
+            [handempty, ontable(b, roomlocation1), ontable(c, roomlocation1), on(a, b, roomlocation1), clear(a, roomlocation1), clear(c, roomlocation1), roomlocation1],
+            [handempty, ontable(b, roomlocation2), on(c, b, roomlocation2), on(a, c, roomlocation2), clear(a, roomlocation2), roomlocation1]
+            ).
+
+/**
+* test3 from Program 2 Operational Description section.
+* test3 is a 0-arity test predicate.
+* The start state is: robot arm in Room 1, all blocks in Room 1, with block B on the table,
+*                     and block A on B.
+* The goal state is: robot arm in Room 1, all blocks in Room 2, with block B on the table,
+*                    and block A on B.
+*/
+test3 :- go(
+            [handempty, ontable(b, roomlocation1), on(a, b, roomlocation1), clear(a, roomlocation1), roomlocation1],
+            [handempty, ontable(b, roomlocation2), on(a, b, roomlocation2), clear(a, roomlocation2), roomlocation1]
+            ).
--- tworoomplanner2.pl
+++ tworoomplanner2.pl
@@ -1,20 +1,16 @@
-
-%%%%%%%%% Simple Prolog Planner %%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%
-%%% Based on one of the sample programs in:
-%%%
-%%% Artificial Intelligence:
-%%% Structures and strategies for complex problem solving
-%%%
-%%% by George F. Luger and William A. Stubblefield
-%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+/*
+* CAP 4630 Artificial Intelligence
+* University of Central Florida
+* Spring 2018
+* Program 2 - Prolog Two Room Planner
+* Written by: Luis Gamarra Jimenez and Jose Sosa
+*/
 
 /* module */
 :- module( planner,
         [
             plan/4,change_state/3,conditions_met/2,member_state/2,
-            move/3,go/2,test/0,testTwoRoomSlides/0
+            move/3,go/2, test1/0, test2/0, test3/0
         ]).
 
 /* utils */
@@ -51,25 +47,25 @@ member_state(S, [_|T]) :- member_state(S, T).
 
 /* move types */
 move(
-     pickup(X, Z),
+     pickup(X),
      [handempty, clear(X, Z), on(X, Y, Z), roomlocation(Z)],
      [del(handempty), del(clear(X, Z)), del(on(X, Y, Z)), add(clear(Y, Z)), add(holding(X))]
     ).
 
 move(
-     pickup(X, Z),
+     pickup(X),
      [handempty, clear(X, Z), ontable(X, Z), roomlocation(Z)],
      [del(handempty), del(clear(X, Z)), del(ontable(X, Z)), add(holding(X))]
     ).
 
 move(
-     putdown(X, Z),
+     putdown(X),
      [holding(X), roomlocation(Z)],
      [del(holding(X)), add(ontable(X, Z)), add(clear(X, Z)), add(handempty)]
     ).
 
 move(
-     stack(X, Y, Z),
+     stack(X, Y),
      [holding(X), clear(Y, Z), roomlocation(Z)],
      [del(holding(X)), del(clear(Y, Z)), add(handempty), add(on(X, Y, Z)), add(clear(X, Z))]
     ).
@@ -89,7 +85,41 @@ move(
 /* run commands */
 go(S, G) :- plan(S, G, [S], []).
 
-testTwoRoomSlides :- go(
-            [handempty, ontable(b,1), on(a, b, 1), clear(a, 1), ontable(c,1), clear(c, 1), roomlocation(1)],
-            [handempty, ontable(c,1), on(b, c, 1), on(a, b, 1), clear(a, 1), roomlocation(1)]
-          ).
+/**
+* test1 from Program 2 Requirements section.
+* test1 is a 0-arity test predicate.
+* The start state is: block A, B, and C and robotic arm are all in Room 1.
+*                     Block B and C on the table and block A on top of block B.
+* The goal state is: all blocks and arm in Room 1, with block C on the table,
+*                    block B on C, and block A on B.
+*/
+test1 :- go(
+            [handempty, ontable(b, 1), ontable(c, 1), on(a, b, 1), clear(a, 1), clear(c, 1), roomlocation(1)],
+            [handempty, ontable(c, 1), on(b, c, 1), on(a, b, 1), clear(a, 1), roomlocation(1)]
+            ).
+
+/**
+* test2 from Program 2 Requirements section.
+* test2 is a 0-arity test predicate.
+* The start state is: block A, B, and C and robotic arm are all in Room 1.
+*                     Block B and C on the table and block A on top of block B.
+* The goal state is: robot arm in Room 1, all blocks in Room 2, with block B on the table,
+                     block C on B, and block A on C.
+*/
+test2 :- go(
+            [handempty, ontable(b, 1), ontable(c, 1), on(a, b, 1), clear(a, 1), clear(c, 1), roomlocation(1)],
+            [handempty, ontable(b, 2), on(c, b, 2), on(a, c, 2), clear(a, 2), roomlocation(1)]
+            ).
+
+/**
+* test3 from Program 2 Operational Description section.
+* test3 is a 0-arity test predicate.
+* The start state is: robot arm in Room 1, all blocks in Room 1, with block B on the table,
+*                     and block A on B.
+* The goal state is: robot arm in Room 1, all blocks in Room 2, with block B on the table,
+*                    and block A on B.
+*/
+test3 :- go(
+            [handempty, ontable(b, 1), on(a, b, 1), clear(a, 1), roomlocation(1)],
+            [handempty, ontable(b, 2), on(a, b, 2), clear(a, 2), roomlocation(1)]
+            ).
